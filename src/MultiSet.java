4f38017bcf2d516a7bcf8099f0dd8bc524f5a401
Parzing <158121522+Parzing@users.noreply.github.com>
2024-09-16 17:15:45 -0400

    added methods for abstract class multiset and documentation

--- src/MultiSet.java
+++ src/MultiSet.java
@@ -10,5 +10,40 @@ public abstract class MultiSet {
      */
     abstract void add(Integer item);
 
-    // TODO finish adding abstract methods to fully describe what it means to be a MultiSet.
+    /**
+     * Remove the given item from this multiset
+     *
+     * @param item the item to remove
+     */
+    abstract void remove(Integer item);
+
+    /**
+     * Returns if an item is in the multiset.
+     *
+     * @param item the item to find
+     * @return whether the item is in the multiset
+     */
+    abstract boolean contains(Integer item);
+
+    /**
+     * Returns if the multiset is empty.
+     *
+     * @return whether the set is empty
+     */
+    abstract boolean isEmpty();
+
+    /**
+     * Count the number of the given item in the multiset.
+     *
+     * @param item the item to count occurrences of
+     * @return Integer the number of occurrences of the item
+     */
+    abstract Integer count(Integer item);
+
+    /**
+     * Gets the size of this multiset.
+     *
+     * @return Size of the multiset
+     */
+    abstract Integer size();
 }
