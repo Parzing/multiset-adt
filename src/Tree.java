5dbf2fc9b31e12bd113320ea4e051671cea88255
Parzing <158121522+Parzing@users.noreply.github.com>
2024-09-17 12:06:37 -0400

    Added blank functions for the Tree class and added methods to the TreeMultiSet class

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,100 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Tree {
-    // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
+    private int root;
+    private ArrayList<Tree> subtrees;
+
+    public Tree(int root, Tree[] subtrees){
+        this.root = root;
+        this.subtrees = new ArrayList<Tree>();
+        this.subtrees.addAll(Arrays.asList(subtrees));
+    }
+    public Tree(int root){
+        this.root = root;
+        this.subtrees = new ArrayList<Tree>();
+    }
+
+    public boolean isEmpty(){
+        // TODO
+        return false;
+    }
+
+    public int length(){
+        // TODO
+        return 0;
+    }
+
+    public int count(int item){
+        // TODO
+        return 0;
+    }
+
+
+    public float average(){
+        // TODO
+        return 0.0f;
+    }
+
+    private int[] average_helper(){
+        // TODO
+        return new int[]{0};
+    }
+
+    public boolean contains(int item){
+        // TODO
+        return false;
+    }
+
+    public int[] leaves(){
+        // TODO
+        return new int[]{};
+    }
+
+    public boolean deleteItem(int item){
+        // TODO
+        return true;
+    }
+
+    private void deleteRoot(){
+        // TODO
+    }
+
+    private int extractLeaf(){
+        // TODO
+        return 0;
+    }
+
+    public void insert(int item){
+        // TODO
+    }
+
+    public boolean insertChild(int item, int parent){
+        // TODO
+        return true;
+    }
+
+
+
+    @Override
+    public String toString(){
+        // TODO
+        return stringIndented(0);
+    }
+
+    private String stringIndented(int depth){
+        return "";
+    }
+
+    @Override
+    public boolean equals(Object other){
+        // TODO
+        return true;
+    }
+
+
+
+
+
+
 }
--- src/TreeMultiSet.java
+++ src/TreeMultiSet.java
@@ -10,6 +10,31 @@ public class TreeMultiSet extends MultiSet {
     void add(Integer item) {
         // TODO complete this method
     }
+
+    @Override
+    void remove(Integer item) {
+
+    }
+
+    @Override
+    boolean contains(Integer item) {
+        return false;
+    }
+
+    @Override
+    boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    Integer count(Integer item) {
+        return null;
+    }
+
+    @Override
+    Integer size() {
+        return null;
+    }
     // TODO write the rest of the implementation for this class so that it uses its private Tree
     //      object to provide the MultiSet functionality.
 }
