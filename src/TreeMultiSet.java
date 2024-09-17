e7d7ae1d5a5258677dc8fab630398ea90b0a44b9
Tyson Caul <tjcaul@icloud.com>
2024-09-17 13:32:29 -0400

    Document TreeMultiSet

--- src/TreeMultiSet.java
+++ src/TreeMultiSet.java
@@ -15,26 +15,53 @@ public class TreeMultiSet extends MultiSet {
         this.tree.insert(item);
     }
 
+    /**
+     * Remove the given item from this multiset.
+     *
+     * @param item the item to remove
+     */
     @Override
     void remove(Integer item) {
         this.tree.deleteItem(item);
     }
 
+    /**
+     * Return whether the multiset includes the given item
+     *
+     * @param item the item to search for
+     * @return whether the item is in the multiset
+     */
     @Override
     boolean contains(Integer item) {
         return this.tree.contains(item);
     }
 
+    /**
+     * Return whether the multiset is empty.
+     *
+     * @return whether the multiset is empty
+     */
     @Override
     boolean isEmpty() {
         return this.tree.isEmpty();
     }
 
+    /**
+     * Return the multiplicity of the given item.
+     *
+     * @param item the item to search for
+     * @return the multiplicity of the item
+     */
     @Override
     Integer count(Integer item) {
         return this.tree.count(item);
     }
 
+    /**
+     * Return the size of the multiset
+     *
+     * @return the total number of items in the multiset
+     */
     @Override
     Integer size() {
         return this.tree.length();
